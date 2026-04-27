119a3808d2e0d2f575a5836334f36d0858fab5b1
pawan-mpcz <pawan.mpcz@gmail.com>
2026-04-27 12:08:05 +0530

    add sms code

--- Accounts/smsapi.py
+++ Accounts/smsapi.py
@@ -1,5 +1,5 @@
 import requests
-proxyDict = {"http": "proxy1.mpcz.in:8080", "https": "proxy1.mpcz.in:8080"}
+proxyDict = {"http": "proxy.mpcz.in:8080", "https": "proxy.mpcz.in:8080"}
 
 #For Login
 def loginOtp(mobile,otp):
@@ -45,121 +45,15 @@ def verifyOtp(mobile,otp):
 		if response:return response.json()
 	except:
 		return {"status":True,"message":"Something Went Wrong "}
-	
-def appraiseeOtp(name,mobile,session):
-	message = "Dear "+name+", Your ACR for "+str(session)+" has been submitted successfully and Forwarded to the Next Authority. - MPMKVVCL"
-	header = {"User-Agent": "Opera/9.80 (X11; Linux x86_64; U; de) Presto/2.2.15 Version/10.00"}
-	url = "https://sms.mpcz.in/api/v1/send-message?app_key=VgsdaQYXmsmVFsUi2Fe4tC7vn&app_secret=dwGINQswrHtyngc&dlt_template_id=1007361279894006110&message=" + str(message) + "&campaign=&mobile_numbers=" +str(mobile)+ "&message_type=1&route_type=0&schedule_date=&is_flash=0"
-	try:
-		response = requests.get(url, headers=header)
-		if response:return response.json()
-	except:
-		return {"status":True,"message":"Something Went Wrong"}
 
-# def reporting_sms():
-# 	mobile = "918827053980"
-# 	name = "Pawan"
-# 	reviewingName = "Sumit"
-# 	# message = "प्रिय "+str(name)+", आपकी ACR रिपोर्ट आपके रिपोर्टिंग अधिकारी द्वारा पूरी कर ली गई है और आपके समीक्षा अधिकारी "+str(reviewingName)+" को भेज दी गई है| Thank You. -MPMKVVCL BHOPAL"
-# 	# message = "प्रिय "+str(name)+", आपकी ACR रिपोर्ट आपके रिपोर्टिंग अधिकारी द्वारा पूरी कर ली गई है और आपके समीक्षा अधिकारी "+str(reviewingName)+" को भेज दी गई है| धन्यवाद. -MPMKVVCL BHOPAL"
-# 	# message = "प्रिय सुमित कुमार, आपकी ACR रिपोर्ट आपके रिपोर्टिंग अधिकारी द्वारा पूरी कर ली गई है और आपके समीक्षा अधिकारी सुरेश कुमार को भेज दी गई है| धन्यवाद. -MPMKVVCL BHOPAL"
-# 	header = {"User-Agent": "Opera/9.80 (X11; Linux x86_64; U; de) Presto/2.2.15 Version/10.00"}
-# 	# url = "https://api.pinnacle.in/index.php/sms/urlsms?sender=CCMPCZ&numbers=8517007090&messagetype=UNI&message="+ str(message) +" &response=Y&dltentityid=1201158039515302745&apikey=210d59-4684c1-525x69-0e1352-5fde17&dlttempid=1007688303325051886"
-# 	url="https://api.pinnacle.in/index.php/sms/urlsms?sender=CCMPCZ&numbers=9926286466&messagetype=UNI&message=प्रिय सुमित कुमार, आपकी ACR रिपोर्ट आपके रिपोर्टिंग अधिकारी द्वारा पूरी कर ली गई है और आपके समीक्षा अधिकारी सुरेश कुमार को भेज दी गई है| धन्यवाद. -MPMKVVCL BHOPAL |&response=Y&dltentityid=1201158039515302745&apikey=210d59-4684c1-525x69-0e1352-5fde17&dlttempid=1007688303325051886"
-# 	print(url,"123456")
-# 	# try:
-# 	response = requests.get(url, proxies=proxyDict, headers=header, verify=False)
-# 	if response:return response.json()
-# # except:
-# 	return {"status":True,"message":"Something Went Wrong"}
-	
-def reviewingOtp(name,mobile, AcceptingName):
-	message =     "प्रिय "+str(name)+", आपकी ACR रिपोर्ट आपके पुनरीक्षण कर्ता अधिकारी द्वारा पूरी कर ली गई है और आपके स्वीकृतकर्ता अधिकारी "+str(AcceptingName)+" को भेज दी गई है । धन्यवाद. -MPMKVVCL BHOPAL"
-	header = {"User-Agent": "Opera/9.80 (X11; Linux x86_64; U; de) Presto/2.2.15 Version/10.00"}
-	url = "https://sms.mpcz.in/api/v1/send-message?app_key=VgsdaQYXmsmVFsUi2Fe4tC7vn&app_secret=dwGINQswrHtyngc&dlt_template_id=1207160595139988353&message=" + str(message) + "&campaign=&mobile_numbers=" +str(mobile)+ "&message_type=1&route_type=0&schedule_date=&is_flash=0"
-	try:
-		response = requests.get(url, headers=header)
-		if response:return response.json()
-	except:
-		return {"status":True,"message":"Something Went Wrong"}
-
-def acceptingOtp(name,mobile, AcceptingName):
-	message = "प्रिय "+str(name)+", आपकी ACR रिपोर्ट को स्वीकृतकर्ता अधिकारी "+str(AcceptingName)+" द्वारा सफलतापूर्वक अनुमोदित कर दिया गया है| धन्यवाद. -MPMKVVCL BHOPAL"
-	header = {"User-Agent": "Opera/9.80 (X11; Linux x86_64; U; de) Presto/2.2.15 Version/10.00"}
-	url = "https://sms.mpcz.in/api/v1/send-message?app_key=VgsdaQYXmsmVFsUi2Fe4tC7vn&app_secret=dwGINQswrHtyngc&dlt_template_id=1207160595117506210&message=" + str(message) + "&campaign=&mobile_numbers=" +str(mobile)+ "&message_type=1&route_type=0&schedule_date=&is_flash=0"
-	try:
-		response = requests.get(url, headers=header)
-		if response:return response.json()
-	except:
-		return {"status":True,"message":"Something Went Wrong"}
-
-# import requests
-# # def reportingOtp(name,mobile,session, reviewingName):
-# message = "Dear sir, Your Appraisal Report for year has been completed by your Reporting Officer and forwarded next to your Reviewing Officer off2 l. Thank you. -MPMKVVCL BHOPAL"
-# header = {"User-Agent": "Opera/9.80 (X11; Linux x86_64; U; de) Presto/2.2.15 Version/10.00"}
-# url = "https://sms.mpcz.in/api/v1/send-message?app_key=VgsdaQYXmsmVFsUi2Fe4tC7vn&app_secret=dwGINQswrHtyngc&dlt_template_id=1207160595156524521&message=" + str(message) + "&campaign=&mobile_numbers=" +str(8517007090)+ "&message_type=1&route_type=0&schedule_date=&is_flash=0"
-# try:
-# 	response = requests.get(url, headers=header)
-# 	print(response)
-
-	
-# 	print("tru")
-# except:
-# 	print("false")
-
-# 	# return {"status":True,"message":"Something Went Wrong"}
-# name,mobile, reviewingName
-# reporting_sms()
-
-
-
-# def send_sms():
-# 			mobile = "918827053980"
-# 			name = "Pawan"
-# 			reviewingName = "Sumit"
-#             message = f"प्रिय सुमित कुमार, आपकी ACR रिपोर्ट आपके रिपोर्टिंग अधिकारी द्वारा पूरी कर ली गई है और आपके समीक्षा अधिकारी सुरेश कुमार को भेज दी गई है| धन्यवाद. -MPMKVVCL BHOPAL |"
-# 			message = f"प्रिय "+str(name)+", आपकी ACR रिपोर्ट आपके रिपोर्टिंग अधिकारी द्वारा पूरी कर ली गई है और आपके समीक्षा अधिकारी "+str(reviewingName)+" को भेज दी गई है| धन्यवाद. -MPMKVVCL BHOPAL |"
-#             # Construct the URL for sending the SMS
-#             url = f"https://api.pinnacle.in/index.php/sms/urlsms?sender=CCMPCZ&numbers=8827053980&messagetype=UNI&message={message}&response=Y&dltentityid=1201158039515302745&apikey=210d59-4684c1-525x69-0e1352-5fde17&dlttempid=1007688303325051886"
-#             # url = f"https://api.pinnacle.in/index.php/sms/urlsms?sender=CCMPCZ&numbers={officer_data.cug_mobile}&messagetype=TXT&message={message}&response=Y&dltentityid=1201158039515302745&apikey=03695d-0794d5-53e943-7x06a9-93ab48&dlttempid=1007674889182348523"
-            
-#             # Define proxy settings (if necessary)
-#             proxyDict = {"http": "proxy1.mpcz.in:8080", "https": "proxy1.mpcz.in:8080"}
-            
-#             # Send the SMS request
-#             sms_response = requests.get(url, proxies=proxyDict, headers={'User-Agent': 'Chrome'}, verify=False)
-            
-#             # Check the response status
-#             if sms_response.status_code == 200:
-#                 response_content = sms_response.text  # Get the response content as text
-#                 # SmsLog.objects.create(mobile_number=officer_data.cug_mobile, message=message, response_message=response_content)
-#                 return {"message": "SMS sent successfully."}  # Return success message
-#             else:
-#                 return {"error": f"Failed to send SMS. Status code: {sms_response.status_code}."}  # Log the error response
-#         # except Officer.DoesNotExist:
-#             return {"error": "Officer not found."}  # Handle the case where the officer is not found
-#         # except ApplicantDetails.DoesNotExist:
-#             return {"error": "Applicant not found."}  # Handle the case where the applicant is not found
-#     # else:
-#     #     return {"error": "Mobile number is missing."}  # Handle missing mobile number
-
-# # reporting_sms()
-# send_sms()
-
-import requests
-import urllib.parse
 
 def send_sms_reporting(name,mobile, reviewingName):
-    # mobile = "918827053980"
-    # name = "Pawan"
-    # reviewingName = "Sumit"
-    # Compose Hindi message
     message = f"प्रिय {name}, आपकी ACR रिपोर्ट आपके रिपोर्टिंग अधिकारी द्वारा पूरी कर ली गई है और आपके समीक्षा अधिकारी {reviewingName} को भेज दी गई है| धन्यवाद. -MPMKVVCL BHOPAL |"
     headers = {'Authorization': 'Bearer fJLnivwnxRXMrhck30MqtlKrw5IUhc15'}
     url = "https://msg.cerfgs.com/pushapi/sendmsg?username=MPMKVVCL_ERP&dest=91"+str(mobile)+"&apikey=rw7g17n0HsI5To0xhfKd6wX8OKY8OqYS&signature=CCMPCZ&msgtype=UNI&msgtxt="+ str(message) +"&entityid=1201158039515302745&templateid=1007688303325051886"
     print("url+++++++++++", url)
     try:
-        response = requests.request("GET", url,proxies=proxyDict, headers=headers,verify=False)
+        response = requests.request("GET", url,proxies=proxyDict, headers=headers)
         print("response:", response.text)
         if response:
             return response.json()
@@ -168,13 +62,13 @@ def send_sms_reporting(name,mobile, reviewingName):
         return {"status": True, "message": "Something Went Wrong"}
 
 
-def send_sms_reviewing(name,mobile, AcceptingName):
-    message = f"प्रिय {name}, आपकी ACR रिपोर्ट आपके पुनरीक्षण कर्ता अधिकारी द्वारा पूरी कर ली गई है और आपके स्वीकृतकर्ता अधिकारी {AcceptingName} को भेज दी गई है | धन्यवाद. -MPMKVVCL BHOPAL |"
+def send_sms_reviewing(name,mobile, ReviewingName):
+    message = f"प्रिय {name}, आपकी ACR रिपोर्ट आपके पुनरीक्षण कर्ता अधिकारी द्वारा पूरी कर ली गई है और आपके स्वीकृतकर्ता अधिकारी {ReviewingName} को भेज दी गई है | धन्यवाद. -MPMKVVCL BHOPAL |"
     headers = {'Authorization': 'Bearer fJLnivwnxRXMrhck30MqtlKrw5IUhc15'}
     url = "https://msg.cerfgs.com/pushapi/sendmsg?username=MPMKVVCL_ERP&dest=91"+str(mobile)+"&apikey=rw7g17n0HsI5To0xhfKd6wX8OKY8OqYS&signature=CCMPCZ&msgtype=UNI&msgtxt="+ str(message) +"&entityid=1201158039515302745&templateid=1007600347917589168"
     print("url+++++++++++", url)
     try:
-        response = requests.request("GET", url, headers=headers)
+        response = requests.request("GET", url,proxies=proxyDict, headers=headers)
         print("response:", response.text)
         if response:
             return response.json()
@@ -182,16 +76,14 @@ def send_sms_reviewing(name,mobile, AcceptingName):
         print("error:", e)
         return {"status": True, "message": "Something Went Wrong"}
 
-# send_sms_reviewing()
-
 
 def send_sms_accepting(name, mobile, AcceptingName):
-    message = f"प्रिय Deepak, आपकी ACR रिपोर्ट को स्वीकृतकर्ता अधिकारी Pawan द्वारा सफलतापूर्वक अनुमोदित कर दिया गया है| धन्यवाद. -MPMKVVCL BHOPAL"
+    message = f"प्रिय {name}, आपकी ACR रिपोर्ट को स्वीकृतकर्ता अधिकारी {AcceptingName} द्वारा सफलतापूर्वक अनुमोदित कर दिया गया है| धन्यवाद. -MPMKVVCL BHOPAL"
     headers = {'Authorization': 'Bearer fJLnivwnxRXMrhck30MqtlKrw5IUhc15'}
     url = "https://msg.cerfgs.com/pushapi/sendmsg?username=MPMKVVCL_ERP&dest=91"+str(mobile)+"&apikey=rw7g17n0HsI5To0xhfKd6wX8OKY8OqYS&signature=CCMPCZ&msgtype=UNI&msgtxt="+ str(message) +"&entityid=1201158039515302745&templateid=1007435805364665975"
     print("url+++++++++++", url)
     try:
-        response = requests.request("GET", url,proxies=proxyDict, headers=headers,verify=False)
+        response = requests.request("GET", url,proxies=proxyDict, headers=headers)
         print("response:", response.text)
         if response:
             return response.json()
